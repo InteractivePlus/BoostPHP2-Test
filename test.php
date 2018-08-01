eea40c53e31211bf54ed1504285bc12cd4385e7b
GuoHaoLan <coopticks@outlook.com>
2018-08-01 13:32:01 +0800

    Update test.php

--- test.php
+++ test.php
@@ -18,8 +18,8 @@ require_once __DIR__ . "/BoostPHP/autoload.php";
 //Start Testing the functions in Cache.php
 
 
-use BoostPHP\Cache\AutoMode as A;
-use BoostPHP\Cache\ManualMode as M;
+use \BoostPHP\Cache\AutoMode as A;
+use \BoostPHP\Cache\ManualMode as M;
 
 var_dump(A::CacheStart(  '200'  ,  '/cache' ,  ));
 
