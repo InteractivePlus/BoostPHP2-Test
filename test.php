f9eadf54678881efd7acae97739d87cfd7f0ed3d
GuoHaoLan <coopticks@outlook.com>
2018-08-01 11:37:16 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+* 
+*  BoostPHP2 in PHP7.1
+*
+*  Test By Liuxing
+*
+*  1st August,2018
+*
+*/ 
+
+
+//Load BoostPHP2 Library
+require_once __DIR__ . "/BoostPHP/autoload.php";
+
+
+//Start Testing the functions in Cache.php
+
+
+use BoostPHP\Cache\AutoMode as A;
+use BoostPHP\Cache\ManualMode as M;
+
+var_dump(A::CacheStart(  '200'  ,  '/cache' ,  ));
+
+
+
+ ?>
